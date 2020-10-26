2e995179d13e652723468a83531a9732c7042547
glenvasa <70590043+glenvasa@users.noreply.github.com>
2020-10-26 17:04:49 -0400

    minor changes

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -13,7 +13,7 @@ export function RegistrationView(props) {
   const [BirthDate, setBirthDate] = useState('');
 
   // Allows login with random credentials for existing user, no functionality for new users yet
-  const handleSubmit = (e) => {
+  const handleRegister = (e) => {
     e.preventDefault();
     console.log(Username, Password, Email, BirthDate);
 
@@ -28,27 +28,28 @@ export function RegistrationView(props) {
       <Form className="registration-form">
         <Form.Group controlId="formBasicUsername" className="registration-item">
           <Form.Label>Create Username: </Form.Label>
-          <Form.Control type="text" placeholder="Username" value={Username} onChange={(e) => setUsername(e.target.value)} />
+          <Form.Control type="text" value={Username} onChange={e => setUsername(e.target.value)} placeholder="Username" />
           <Form.Text className="text-muted">Must be alphanumberic and have a minimum of 8 characters.</Form.Text>
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword" className="registration-item">
           <Form.Label>Create Password: </Form.Label>
-          <Form.Control type="text" placeholder="Password" value={Password} onChange={(e) => setPassword(e.target.value)} />
+          <Form.Control type="password" value={Password} onChange={e => setPassword(e.target.value)} placeholder="Password" />
           <Form.Text className="text-muted">Must be alphanumberic and have 8-15 characters.</Form.Text>
         </Form.Group>
 
         <Form.Group controlId="formBasicEmail" className="registration-item">
           <Form.Label>Enter Email Address: </Form.Label>
-          <Form.Control type="text" placeholder="Email Address" value={Email} onChange={(e) => setEmail(e.target.value)} />
+          <Form.Control type="email" placeholder="Email Address" value={Email} onChange={e => setEmail(e.target.value)} />
         </Form.Group>
 
         <Form.Group controlId="formBasicBirthdate" className="registration-item">
           <Form.Label>Enter Date of Birth: </Form.Label>
-          <Form.Control type="text" placeholder="YYYY-MM-DD" value={BirthDate} onChange={(e) => setBirthDate(e.target.value)} />
+          <Form.Control type="date" placeholder="YYYY-MM-DD" value={BirthDate} onChange={e => setBirthDate(e.target.value)} />
         </Form.Group>
 
-        <Button type="button" onClick={handleSubmit}>Submit</Button>
+        <Button type="submit" variant="primary" className="button-registration" onClick={handleRegister}>Submit</Button>
+        <Link to={"/"}><Button variant="secondary" className="button-cancel">Cancel</Button></Link>
       </Form>
     </div>
   );
@@ -59,6 +60,6 @@ RegistrationView.propTypes = {
     Username: PropTypes.string.isRequired,
     Password: PropTypes.string.isRequired,
     Email: PropTypes.string.isRequired,
-    Birtdate: PropTypes.instanceOf(Date).isRequired
+    Birthdate: PropTypes.instanceOf(Date).isRequired
   })
 };
