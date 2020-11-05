b4f1681593a28d8bfc48689e0daf9035b8303149
glenvasa <70590043+glenvasa@users.noreply.github.com>
2020-11-05 11:14:00 -0500

    Added buttons to link to genreview and directorview

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import PropTypes from 'prop-types';
 import './movie-view.scss'
@@ -12,7 +13,7 @@ export class MovieView extends React.Component {
   }
 
   render() {
-    const { movie, onClick } = this.props;
+    const { movie } = this.props;
 
     if (!movie) return null;
 
@@ -31,12 +32,20 @@ export class MovieView extends React.Component {
           <div className="movie-genre">
             <span className="label">Genre: </span>
             <span className="value">{movie.Genre.Name}</span>
+            <Link to={`/genres/${movie.Genre.Name}`}>
+              <Button variant="link" className="genre-button" style={{ background: '#690f38' }}>Genre Info</Button>
+            </Link>
           </div>
           <div className="movie-director">
             <span className="label">Director: </span>
             <span className="value">{movie.Director.Name}</span>
+            <Link to={`/directors/${movie.Director.Name}`}>
+              <Button variant="link" className="director-button" style={{ background: '#690f38' }}>Director Info</Button>
+            </Link>
           </div>
-          <Button className="back-button" style={{ background: '#690f38' }} onClick={() => onClick()}>Go Back</Button>
+          <Link to={"/"}>
+            <Button className="back-button" style={{ background: '#690f38' }} >Go Back</Button>
+          </Link>
         </section>
       </div >
     );
@@ -50,13 +59,15 @@ MovieView.propTypes = {
     ImagePath: PropTypes.string.isRequired,
     Genre: PropTypes.shape({
       Name: PropTypes.string.isRequired,
-      Description: PropTypes.string.isRequired
+      Description: PropTypes.string.isRequired,
+      ImagePath: PropTypes.string
     }),
     Director: PropTypes.shape({
       Name: PropTypes.string.isRequired,
       Bio: PropTypes.string.isRequired,
       Birth: PropTypes.string.isRequired,
-      Death: PropTypes.string
+      Death: PropTypes.string,
+      ImagePath: PropTypes.string
     }),
   })
 }
